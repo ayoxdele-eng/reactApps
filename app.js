156c0a6c8be12b559b5530cb05b3cd38f1be52a0
ayoxdele-eng <70418626+ayoxdele-eng@users.noreply.github.com>
2023-11-06 15:48:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup used
+// view engine setup used for the project
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
